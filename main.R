3fa5c8832a3d85a5d47b166593bec5830c06975e
@daaronr <daaronr@gmail.com>
2021-11-17 11:26:55 -0500

    add trackdown command examples
    @Jimbilben @WillemSleegers  The commands at bottom of `main.R` worked for me to move the content to the shared google doc
    
    (after installing the stuff commented out above)
    
    Wonder if it works for you.
    
    @kcudding -- you will also dig this trackdown thing!

--- main.R
+++ main.R
@@ -49,6 +49,11 @@ source(here::here("code", "project_setup.R"))
 ##NOTE: these sourced files seem to need some packages to be installed.
 #Todo -- 'embed' that somehow? (I just used Renv to add these for now)
 
+
+#remotes::install_github("claudiozandonella/trackdown", build_vignettes = TRUE)
+# trying 'trackdown' (https://bookdown.org/yihui/rmarkdown-cookbook/google-drive.html) to help collaborate dynamically
+#library(trackdown)
+
 source(here::here("code", "download_formatting.R"))
 
 print("project_setup creates 'support' folder and downloads tufte_plus.css, header.html into it")
@@ -62,7 +67,7 @@ print("project_setup creates 'code' folder and downloads baseoptions.R, and func
 
 dir.create(here("remote"))
 
-#THIS fails, probably because its a private repo: try_download("https://raw.githubusercontent.com/rethinkpriorities/ea-data/master/Rmd/methods_interaction_sharing.Rmd?token=AB6ZCMD4HRHLJCFNLBKYO5LBRWHLY", here::here("remote", "methods_interaction_sharing_remote.Rmd"))
+#THIS fails, probably because its a private repo: try_download("https://raw.githubusercontent.com/rethinkpriorities/ea-data/master/Rmd/methods_interaction_sharing.Rmd?token=AB6ZCMD4HRHLJCFNLBKYO5LBRWHLY", here::here("remote", "methods_interaction_sharing_remote.Rmd"))s
 
 
 p_load("bettertrace") #better tracking after bugs
@@ -76,6 +81,20 @@ p_load("bettertrace") #better tracking after bugs
   options(knitr.duplicate.label = "allow")
   rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 }
+
+
+# trackdown command examples ####
+
+#p_load(googledrive)
+#remotes::install_github("claudiozandonella/trackdown", build_vignettes = TRUE)
+#library(trackdown)
+
+#see https://app.getguru.com/card/cd469abi/collab-writing-sessions-working-this-into-Github-and-Rmarkdown and https://rethinkpriorities.slack.com/archives/C027CUXNQTD/p1637074537043600 and https://claudiozandonella.github.io/trackdown/
+
+trackdown::upload_file(
+  file = here("power_analysis_framework_2_COLLAB.Rmd"), 
+  shared_drive = "Research", #this works -- name looked up with googledrive::shared_drive_find()
+  hide_code = FALSE) #hide_code=TRUE is usually better but I want to see it for now
 
 
 
